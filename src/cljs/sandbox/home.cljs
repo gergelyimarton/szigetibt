98dcdd38a1595f77f0bd4a498e842a8ada4dde43
Gergelyi Márton <gergelyimarci@gmail.com>
2022-09-05 20:14:42 +0200

    service pictures and buttons fixed!

--- src/cljs/sandbox/home.cljs
+++ src/cljs/sandbox/home.cljs
@@ -184,35 +184,29 @@
 
 
 ;; ------------------------ kezeléshez tartozó képek és inline css
+(def kep-style {
+                :border-radius "10px"
+                :width "80%"
+                :margin "20px"
+                :margin-top "100px"})
+
 
 (defn elso-kep []
   [:img {:src "/images/egy7.jpg"
-              :style {
-                      :border-radius "10px"
-                      :width "73%"
-                      :margin "20px"}}])
+         :style kep-style}])
 
 
 (defn masodik-kep []
   [:img {:src "/images/ketto.jpg"
-              :style {
-                      :border-radius "10px"
-                      :width "80%"
-                      :margin "20px"}}])
+         :style kep-style}])
 
 (defn harmadik-kep []
   [:img {:src "/images/harom.jpg"
-              :style {
-                      :border-radius "10px"
-                      :width "80%"
-                      :margin "20px"}}])
+         :style kep-style}])
 
 (defn negyedik-kep []
   [:img {:src "/images/eszter.jpg"
-              :style {
-                      :border-radius "10px"
-                      :width "80%"
-                      :margin "20px"}}])
+         :style kep-style}])
 
 
 
@@ -233,23 +227,19 @@
            [:div "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]]
 
 ;; -------------------------------- képek gombokkal
-          [:div.three-pictures-text
-           [:div
-            [:div
-             [elso-kep]]
-            [kezeles-button 0]]
-           [:div
-            [:div
-             [masodik-kep]]
-            [kezeles-button 1]]
-           [:div
-            [:div
+          [:div.services-grid
+           [:div.service-container
+              [elso-kep]
+              [kezeles-button 0]]
+           [:div.service-container
+             [masodik-kep]
+             [kezeles-button 1]]
+           [:div.service-container
               [harmadik-kep]
-             [kezeles-button 2]]]
-           [:div
-            [:div
-             [negyedik-kep]]
-            [kezeles-button 3]]]
+              [kezeles-button 2]]
+           [:div.service-container
+             [negyedik-kep]
+             [kezeles-button 3]]]
 ;; ------------------------------- kezelések leírása
           [:div.kezelesek-outer
            [:div#kezelesek-mid
